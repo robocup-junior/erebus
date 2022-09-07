e133548b8092495c7890173e67bb0afaee1734de
Alfred Roberts <robertsalfie14@gmail.com>
2022-09-07 16:06:47 +0100

    Update follow camera positions

--- game/controllers/MainSupervisor/Camera.py
+++ game/controllers/MainSupervisor/Camera.py
@@ -1,34 +1,39 @@
-
+from ConsoleLog import Console
 class Camera():
     def __init__(self, node, side="bottom"):
         self.wb_viewpoint_node = node
         self.side = side
         
     def setViewPoint(self, robotObj):
+        Console.log_info(str(robotObj.position))
         if self.side == "top":
+            Console.log_info("Top")
             vp = [
                 robotObj.position[0],
                 robotObj.position[1] + 0.8,
-                robotObj.position[2] - 0.8
+                robotObj.position[2] - 0.5
             ]
             vo = [-0.301184, -0.299742, -0.905231, 1.66967]
         elif self.side == "right":
+            Console.log_info("Right")
             vp = [
-                robotObj.position[0] + 0.8,
+                robotObj.position[0] + 0.7,
                 robotObj.position[1] + 0.8,
                 robotObj.position[2]
             ]
             vo = [0.326219, -0.668033, -0.668814, 2.50804]
         elif self.side == "bottom":
+            Console.log_info("Bottom")
             vp = [
                 robotObj.position[0],
                 robotObj.position[1] + 0.8,
-                robotObj.position[2] + 0.8
+                robotObj.position[2] + 0.7
             ]
             vo = [-0.683263, 0.683263, 0.257493, 2.63756]
         elif self.side == "left":
+            Console.log_info("left")
             vp = [
-                robotObj.position[0] - 0.8,
+                robotObj.position[0] - 0.5,
                 robotObj.position[1] + 0.8,
                 robotObj.position[2]
             ]
