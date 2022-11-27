970114078e989e49a378d21ac9aa3f1be8c85011
Alfred Roberts <robertsalfie14@gmail.com>
2022-11-27 12:07:14 +0000

    Fix world change bug
    Fixed bug where the first entry in the world list wouldn't load

--- game/controllers/MainSupervisor/RobotWindowSender.py
+++ game/controllers/MainSupervisor/RobotWindowSender.py
@@ -7,9 +7,9 @@ class RWSender:
         self.history[command] = args
     
     def send(self,command: str, args: str = ''):
-        self.supervisor.wwiSendText(command + ', ' + args)
+        self.supervisor.wwiSendText(command + ',' + args)
         self.updateHistory(command, args)
     
     def sendAll(self):
         for command in self.history:
-            self.supervisor.wwiSendText(command + ', ' + self.history[command])
+            self.supervisor.wwiSendText(command + ',' + self.history[command])
