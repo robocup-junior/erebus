1fd07be1af6e84fdb87a409ee59d28399bdef32c
Alfred Roberts <robertsalfie14@gmail.com>
2023-01-24 10:41:47 +0000

    Fix console log colours
    No longer using termcolor since its not compatible
    with Webots terminal.

--- game/controllers/MainSupervisor/ConsoleLog.py
+++ game/controllers/MainSupervisor/ConsoleLog.py
@@ -1,7 +1,10 @@
 import AutoInstall
 AutoInstall._import("cl", "termcolor")
+import sys
+import os
 
 class Console:
+    PREFIX_DEBUG: str = "EREBUS DEBUG"
     PREFIX_PASS: str = "EREBUS PASS"
     PREFIX_FAIL: str = "EREBUS FAIL"
     PREFIX_ERROR: str = "EREBUS ERROR"
@@ -13,6 +16,27 @@ class Console:
     COLOR_WARN: str = "magenta"
     COLOR_SUCC: str = "green"
     COLOR_INFO: str = "blue"
+    COLOR_DEBUG: str = "yellow"
+    
+    COLORS = dict(
+        list(
+            zip(
+                [
+                    "grey",
+                    "red",
+                    "green",
+                    "yellow",
+                    "blue",
+                    "magenta",
+                    "cyan",
+                    "white",
+                ],
+                list(range(30, 38)),
+            )
+        )
+    )
+    
+    RESET = "\033[0m"
 
     @staticmethod
     def log_err(msg: str) -> None:
@@ -37,9 +61,14 @@ class Console:
     @staticmethod
     def log_info(msg: str) -> None:
         Console._log(Console.PREFIX_INFO, msg, Console.COLOR_INFO)
+        
+    @staticmethod
+    def log_debug(msg: str) -> None:
+        Console._log(Console.PREFIX_DEBUG, msg, Console.COLOR_DEBUG)
     
     @staticmethod
     def _log(prefix: str, msg: str, color: str):
         lines = msg.split("\n")
         for line in lines:
-            print(cl.colored(f"[{prefix}] {line}", color))
+            # TODO remove colour prefix for stdout isn't a terminal...
+            print(f"\033[{Console.COLORS[color]}m[{prefix}] {line}" + Console.RESET)
