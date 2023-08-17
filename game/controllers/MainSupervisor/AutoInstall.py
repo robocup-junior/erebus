1b57da323ad22a9d24df97bcb3e9607ca4ba475f
Alfred Roberts <robertsalfie14@gmail.com>
2023-08-17 11:28:58 +0100

    Update AutoInstall docs

--- game/controllers/MainSupervisor/AutoInstall.py
+++ game/controllers/MainSupervisor/AutoInstall.py
@@ -2,7 +2,21 @@ from pip._internal import main as _main
 import importlib
 import inspect
 
-def _import(name, importModule, installModule=None, ver=None):
+def _import(
+        name: str,
+        importModule: str,
+        installModule: None | str = None,
+        ver: None | str = None
+    ) -> None:
+    """Import and install python modules automatically
+
+    Args:
+        name (str): Name to reference module as (e.g. import numpy as np)
+        importModule (str): Pip module name to import
+        installModule (None | str, optional): Pip module name to install. Leave 
+            blank (default None) to install the same module as is imported.
+        ver (None | str, optional): Specify module version. Defaults to None.
+    """
     try:
         inspect.stack()[1][0].f_globals[name] = importlib.import_module(importModule)
     except ImportError:
