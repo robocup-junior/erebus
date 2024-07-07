29a570ce903cddc0ec2aa1b5d5f5d050b66dce8d
Alfred Roberts <robertsalfie14@gmail.com>
2024-07-07 16:49:13 +0100

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,7 @@ All notable changes to this project will be documented in this file
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 
-## [Release v24.0.1]() - 2024-07-XX
+## [Release v24.1.0]() - 2024-07-XX
 
 ### Changed
 
--- game/controllers/MainSupervisor/MainSupervisor.py
+++ game/controllers/MainSupervisor/MainSupervisor.py
@@ -61,7 +61,7 @@ class Erebus(Supervisor):
 
         # Version info
         self._stream = 24
-        self.version = "24.0.1"
+        self.version = "24.1.0"
 
         # Start controller uploader
         uploader: Thread = Thread(target=ControllerUploader.start, daemon=True)
